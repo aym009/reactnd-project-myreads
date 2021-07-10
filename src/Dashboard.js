17b4d00971fdc66a05418ff4741fe95b9b5041da
aym009 <aym.ine.012009@gmail.com>
2021-07-10 14:18:04 +0200

    Add routing

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Dashboard extends Component {
   render() {
@@ -159,7 +160,7 @@ class Dashboard extends Component {
           </div>
         </div>
         <div className="open-search">
-          <button>Add a book</button>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     )
