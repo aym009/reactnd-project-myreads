69d8ebbeab7244181f7519c09b44796b07261903
aym009 <aym.ine.012009@gmail.com>
2021-07-11 17:02:08 +0200

    Create BookShelf and Book components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks'
 import Dashboard from './Dashboard'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books
+        }))
+      })
+  }
+
   render() {
+    const { books } = this.state
+
     return (
       <div className="app">
-        <Route exact path='/' component={Dashboard} />
+        <Route exact path='/' render={() => (
+          <Dashboard books={books} />
+        )} />
         <Route path='/search' component={SearchBooks} />
       </div>
     )
